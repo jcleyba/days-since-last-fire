af146a578b9ca5d3db9f079dfc683992cb855dc0
Juan Cruz Leyba <jcruzleyba@rbi.com>
2022-11-06 21:30:41 -0300

    using other dep for supabase

--- db/client.ts
+++ db/client.ts
@@ -1,6 +1,6 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "https://deno.land/x/supabase@1.3.1/mod.ts";
 
 const supabaseUrl = Deno.env.get("SUPABASE_URL") || "";
 const supabaseKey = Deno.env.get("SUPABASE_ANON_KEY") || "";
 
-export const dbClient = createClient(supabaseUrl, supabaseKey, "");
+export const dbClient = createClient(supabaseUrl, supabaseKey);
